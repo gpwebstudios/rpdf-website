8404b11e212266c3b4656b0b492e2b2243ad8618
zlaughery <zlaughery9@gmail.com>
2022-12-01 18:16:50 -0500

    adjust grid for mobile

--- pages/press.tsx
+++ pages/press.tsx
@@ -7,27 +7,27 @@ import styles from "../styles/Press.module.scss";
 
 const newsArticles = [
     {
-        date: 'October 5, 2022', 
-        title: 'Raleigh Police Host Coffee with a Cop', 
-        description: 'In an effort to increase transparency, engage in the community and develop mutual trust and respect, Raleigh police officers on Wednesday are spread out across the city hoping to chat with the community.', 
+        date: 'October 5, 2022',
+        title: 'Raleigh Police Host Coffee with a Cop',
+        description: 'In an effort to increase transparency, engage in the community and develop mutual trust and respect, Raleigh police officers on Wednesday are spread out across the city hoping to chat with the community.',
         link: 'https://www.wral.com/raleigh-police-host-coffee-with-a-cop-to-break-down-barriers/20507993/'
     },
     {
-        date: 'November 5, 2022', 
-        title: 'RPDF Purchases New Equipment for RPD', 
-        description: 'Courtesy of generation donations from RPDF supporters, the RPDF was able to purchase new equipment for the RPD.', 
+        date: 'November 5, 2022',
+        title: 'RPDF Purchases New Equipment for RPD',
+        description: 'Courtesy of generation donations from RPDF supporters, the RPDF was able to purchase new equipment for the RPD.',
         link: 'https://www.google.com'
     },
     {
-        date: 'December 5, 2022', 
-        title: 'RPDF Hosts 1st Annual Holiday Party', 
-        description: 'RPDF hosted its first annual holiday party for RPD officers and their families.', 
+        date: 'December 5, 2022',
+        title: 'RPDF Hosts 1st Annual Holiday Party',
+        description: 'RPDF hosted its first annual holiday party for RPD officers and their families.',
         link: 'https://www.google.com'
     },
     {
-        date: 'January 5, 2023', 
-        title: 'Raleigh Police Save Kittens', 
-        description: 'Raleigh police officers saved a litter of kittens from a burning building.', 
+        date: 'January 5, 2023',
+        title: 'Raleigh Police Save Kittens',
+        description: 'Raleigh police officers saved a litter of kittens from a burning building.',
         link: 'https://www.google.com'
     },
 ]
@@ -43,21 +43,23 @@ const Press = () => {
                     <h4>The latest news around the Raleigh Police Department Foundation</h4>
                 </div>
                 <div className={`container border-bottom border-top`}>
-                    <div className="my-5 g-5">
-                        {newsArticles.map((article, index) => (
-                            <div key={index}>
-                                <div className={`${styles.news} ${index % 2 === 0 ? '' : `${styles.right}`} ${index % 2 === 1 ? '' : `${styles.left}`} bg-white border border-light rounded-1 p-3 shadow-sm overflow-auto position-relative`}>
-                                    <p className={`${styles.date}`}>{article.date}</p>
-                                    <div>
-                                        <h5 className="">{article.title}</h5>
-                                        <div className={`${styles.textLightGray}`}>
-                                            {article.description}
+                    <div className=" my-5 g-5">
+                        <div className="row row-align-center">
+                            {newsArticles.map((article, index) => (
+                                <div className="col-md-6" key={index}>
+                                    <div className={`${styles.news} ${index % 2 === 0 ? '' : `${styles.right}`} ${index % 2 === 1 ? '' : `${styles.left}`} bg-white border border-light rounded-1 p-3 shadow-sm`}>
+                                        <p className={`${styles.date}`}>{article.date}</p>
+                                        <div>
+                                            <h5 className="">{article.title}</h5>
+                                            <div className={`${styles.textLightGray} pb-2`}>
+                                                {article.description}
+                                            </div>
+                                            <a className={`${styles.link}`} href={article.link}>Read full story</a>
                                         </div>
-                                        <a className={`${styles.link}`} href={article.link}>Read full story</a>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
