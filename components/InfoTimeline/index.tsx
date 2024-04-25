aaf124cd66362ee07006c7e420791cd28be79ce0
zlaughery <zlaughery9@gmail.com>
2024-04-25 09:57:38 -0400

    Update landing page with new copy

--- components/InfoTimeline/index.tsx
+++ components/InfoTimeline/index.tsx
@@ -14,27 +14,27 @@ import styles from "./info-timeline.module.scss";
 const pageData = [
   {
     imageSrc: tl1,
-    description: "The RPDF is an independent, non-profit organization dedicated to assuring safer neighborhoods and highly-skilled and equipped police through better technology, education and programming.",
-    buttonText: "Play Video",
+    description: "The RPDF is a non-profit organization dedicated to empowering police with advanced technology, top-notch education, and innovative programs. A well-equipped and well-trained police force means a safer tomorrow, and a safer community, for everyone.",
+    buttonText: "Learn More",
     btnHref: '/'
   },
   {
     imageSrc: tl2,
-    description: "The RPDF funds critical equipment and initiatives designed to support a thriving community.",
-    buttonText: "Our Work",
+    description: "The RPDF helps to equip our police force with the resources they need to keep our communities safer and more secure through donations of critical equipment and initiatives that make a real difference.",
+    buttonText: "See Our Efforts in Action",
     btnHref: '/Our-Work'
 
   },
   {
     imageSrc: tl3,
-    description: "The RPDF aims to protect our community by strengthening the Raleigh Police Department services and promoting public safety.",
-    buttonText: "About Us",
+    description: "At its core, the RPDF is driven by a steadfast commitment to nurturing a safer, more secure Raleigh for all. Through forward-thinking initiatives and collaborative partnerships, we empower the Raleigh Police Department with the resources and support needed to elevate their capabilities. Our mission is to champion proactive measures that fortify public safety, fostering an environment where our community can thrive without fear.",
+    buttonText: "About the RDPF",
     btnHref: '/about'
   },
   {
     imageSrc: tl4,
-    description: "Take a look behind the badge and meet the inspiring officers who work tirelessly to protect our community.",
-    buttonText: "The Faces of The RPD",
+    description: "Take a look behind the badge and meet the dedicated men and women of the Raleigh Police Department who work tirelessly to protect our communities. These are the incredible local heroes who answer the call, without hesitation, when our safety is on the line.",
+    buttonText: "Meet Our Local Heroes",
     btnHref: '/Meet-RPD'
   },
 ]
@@ -51,8 +51,8 @@ const InfoTimeline = () => {
             <div className="row text-center align-items-center" key={index}>
               <div className="col-md-6">
                 <div
-                  className={`${styles.imageWrapper} ${data.buttonText === 'Play Video' ? 'p-5' : 'p-5'} m-auto `}
-                  onClick={() => data.buttonText === 'Play Video' && setModalShow(true)}>
+                  className={`${styles.imageWrapper} ${data.buttonText === 'Learn More' ? 'p-5' : 'p-5'} m-auto `}
+                  onClick={() => data.buttonText === 'Learn More' && setModalShow(true)}>
                   <Image src={data.imageSrc} alt="RPDF Logo" layout="responsive" />
                 </div>
               </div>
@@ -60,7 +60,7 @@ const InfoTimeline = () => {
                 <div className={`${styles.sectionDescription} pb-4 m-auto`}>
                   {data.description}
                 </div>
-                {data.buttonText === 'Play Video'
+                {data.buttonText === 'Learn More'
                   ?
                   <Button className="px-5" variant="primary" onClick={() => setModalShow(true)}>
                     <div className="d-flex align-items-center">
