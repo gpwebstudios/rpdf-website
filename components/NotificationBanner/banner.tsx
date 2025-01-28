5059c85461946986059c23e8457eb8ceb74b41d9
zlaughery <zlaughery9@gmail.com>
2025-01-28 16:42:59 -0500

    Update banner wording

--- components/NotificationBanner/banner.tsx
+++ components/NotificationBanner/banner.tsx
@@ -5,8 +5,8 @@ const NotificationBanner = () => {
     return (
         <div style={{ textAlign: 'center', fontSize: '1.25rem' }}>
         <Alert variant='danger'>
-            To donate now for Officer Gillick that was recently injured on duty, use this {' '}
-            <Alert.Link href="https://www.paypal.com/donate/?hosted_button_id=W37M2CD6UA62Y">link</Alert.Link>. 
+            Donate to support Officer Gillick, recently injured in the line of duty {' '}
+            <Alert.Link href="https://www.paypal.com/donate/?hosted_button_id=W37M2CD6UA62Y">here</Alert.Link>. 
         </Alert>
         </div>
     );
